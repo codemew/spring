9c3f82f6b1c3043c895ec7a8045e483ee21539fa
somna <somnath.banik911@gmail.com>
2022-08-28 21:02:49 +0530

    Updated

--- src/main/java/com/eazybytes/eazyschool/repository/CoursesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eazybytes.eazyschool.repository;
-
-import com.eazybytes.eazyschool.model.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoursesRepository extends JpaRepository<Courses,Integer> {
-}
